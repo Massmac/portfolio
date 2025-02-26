bfaefe031433137e9c7c6415002cfd34b8158a7a
Michael Tamatey <160445316+Massmac@users.noreply.github.com>
2025-02-26 15:00:17 -0400

    fix: update footer font sizes for better readability and enhance project summaries with detailed descriptions and technology used

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,29 +1,38 @@
 import React from "react";
 import "./Projects.css";
-import massEn from "../../assets/projects/massEn.jpeg";
+import massEn from "../../assets/projects/massEnterprise.png";
 import pressureWashing from "../../assets/projects/pWashing.png";
 
 const projectsData = [
   {
     title: "Mass Enterprise Inventory System",
-    imageSrc: massEn, // Replace with your image path
+    imageSrc: massEn,
     summary:
-      "This is a web application that enables users to schedule and managed" +
-      "residential and commercial pressure washing services.",
+      "A web-based inventory management system for Mass Enterprise, designed to streamline the management of electronic products." +
+      "The application provides business owners with an intuitive interface to add, update, and delete products, customers, and orders, track inventory levels in real time, and process secure online payments." +
+      "Additionally, the system offers monthly income tracking, detailed reporting, and role-based access control for Admins and Employees.",
+    techUsed:
+      "Technologies used: Java, Spring Boot, MySQL, Spring Security, TypeScript, React, HTML, CSS ",
     caseStudyLink: "projects/one",
   },
   {
     title: "Pressure Washing - Mobile Application",
-    imageSrc: pressureWashing, // Replace with your image path
+    imageSrc: pressureWashing,
     summary:
-      "This project focuses on solving a specific problem using advanced techniques and tools.",
+      "An Android mobile application counterpart the Pressure Washing Service Platform, enabling users to book and manage residential and commercial pressure washing services. " +
+      "The app integrates seamlessly with the web platform, providing features for selection, booking management, payment processing, and an admin dashboard for monitoring revenue and client requests.",
+    techUsed: "Technologies: Kotlin, Android Studio,Java, REST APIs",
     caseStudyLink: "projects/two",
   },
   {
     title: "Pressure Washing - Web Application",
-    imageSrc: pressureWashing, // Replace with your image path
+    imageSrc: pressureWashing,
     summary:
-      "An innovative project that addresses real-world challenges in an efficient way.",
+      "A web Application enabling users to schedule and manage residential and commercial pressure washing services. " +
+      "The application supports service selection, views, and updates appointment booking. " +
+      "It includes an admin dashboard for business owners to monitor requests, client details, and revenue, with a feature to calculate monthly income based on bookings and discounts.",
+    techUsed:
+      "Technologies: Java, Spring Boot, MySQL, HTML, CSS, JDBC, File I/O ",
     caseStudyLink: "projects/three",
   },
 ];
@@ -42,6 +51,7 @@ const Projects: React.FC = () => {
             />
             <h3>{project.title}</h3>
             <p>{project.summary}</p>
+            <p>{project.techUsed}</p>
             <a href={project.caseStudyLink || ""} className="case-study-link">
               View Case Study
             </a>
