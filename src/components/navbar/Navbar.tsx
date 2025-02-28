f57e3098d598d05e743248f7397882111c104c30
Michael Tamatey <160445316+Massmac@users.noreply.github.com>
2025-02-28 09:30:04 -0400

    fix: enhance navbar responsiveness with mobile menu toggle and styling adjustments

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,36 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import closeNav from "../../assets/nav/closeIcon.png";
+import menuIcon from "../../assets/nav/menuIcon.png";
 
 const Navbar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <ul className="nav-menu">
-          <li className="nav-item">
+        {/* Menu Toggle Button */}
+        <img
+          className="menu-btn"
+          src={isMenuOpen ? closeNav : menuIcon}
+          alt="menu-button"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
+
+        {/* Navigation Menu */}
+        <ul
+          className={`nav-menu ${isMenuOpen ? "menu-open" : ""}`}
+          onClick={() => setIsMenuOpen(false)}
+        >
+          <li className="nav-item1">
             <Link to="/" className="nav-link">
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="projects/one" className="nav-link">
+            <Link to="/projects/one" className="nav-link">
               Project One
             </Link>
-            <li className="nav-item">
-              <Link to="projects/two" className="nav-link">
-                Project Two
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="projects/three" className="nav-link">
-                Project Three
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="./Contact" className="nav-link">
-                Contact Me
-              </Link>
-            </li>
+          </li>
+          <li className="nav-item">
+            <Link to="/projects/two" className="nav-link">
+              Project Two
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/projects/three" className="nav-link">
+              Project Three
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link">
+              Contact Me
+            </Link>
           </li>
         </ul>
       </div>
